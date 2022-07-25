306992487b4e3461bf969f6f70796d7364fa3671
ILLIA DOBROVOLSKYI <illia.gopro@gmail.com>
2022-07-25 15:31:21 +0300

    ss

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+
+
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  while(/\(\)|\[\]|\{\}|\|\|/g.test(str)){ // while there are still any of these left
+    str = str.replace(/\(\)|\[\]|\{\}|\|\|/g,"") // keep replacing with ''
+}
+return !str.length; // return the opposite of brace.length after that 
+// 0 becomes 1 and 1 becomes 0
 }
